81baacd805d35dc2d46834b10fe8819300dd6fed
Emzy-Jayyy <emediongb23@gmail.com>
2025-12-10 17:11:26 +0100

    Update database.config.ts

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,14 +6,24 @@ export const getDatabaseConfig = (
 ): TypeOrmModuleOptions => {
   return {
     type: 'postgres',
-    host: configService.get<string>('DB_HOST'),
-    port: configService.get<number>('DB_PORT') || 5432,
-    username: configService.get<string>('DB_USERNAME'),
-    password: configService.get<string>('DB_PASSWORD') || '', // Ensure it's a string
-    database: configService.get<string>('DB_NAME'),
+    url: configService.get<string>('DATABASE_URL'),
+
+    // host: configService.get<string>('DB_HOST'),
+    // port: configService.get<number>('DB_PORT') || 5432,
+    // username: configService.get<string>('DB_USERNAME'),
+    // password: configService.get<string>('DB_PASSWORD') || '', // Ensure it's a string
+    // database: configService.get<string>('DB_NAME'),
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    autoLoadEntities: true,
+
     synchronize: configService.get<string>('NODE_ENV') !== 'production',
+
     migrationsRun: true,
+    migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+
     logging: process.env.NODE_ENV !== 'production',
+    ssl: {
+      rejectUnauthorized: false,
+    },
   };
 };
